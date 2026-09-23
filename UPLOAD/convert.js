af0fa5f15d1580731570fcd9718b5b9109120e3d
Woody Lin <wancheng.lin001@gmail.com>
2026-09-23 11:45:04 -0400

    ...

--- UPLOAD/convert.js
+++ UPLOAD/convert.js
@@ -78,4 +78,4 @@ async function convertImages(
 }
 
 // Run the conversion - just edit these values as needed:
-convertImages(8089, "https://woodylinwc.github.io/Image-Storage/GIDLE/");
+convertImages(8421, "https://woodylinwc.github.io/Image-Storage/GIDLE/");
